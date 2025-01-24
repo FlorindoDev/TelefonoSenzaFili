779b9e42626b4cc49eb504b796bc907a3dc1a693
Florindo Zecconi <81627311+FlorindoDev@users.noreply.github.com>
2025-01-24 18:58:58 +0100

    errori di battitura

--- main.c
+++ main.c
@@ -4,15 +4,13 @@
 
 #include "include/connectionDB.h"
 #include "include/auth.h"
-<<<<<<< HEAD
 #include "Prove Socket/server.c"
-=======
->>>>>>> c409c2b5b7b82bf9ca566b88c23c05a0d329f410
+
 
 
 int main(){
 
-<<<<<<< HEAD
+
     
 
     //Utente user;
@@ -25,19 +23,7 @@ int main(){
 
     //register_user(conn,&user);
     mainServer();
-=======
-    PGconn* conn=connect_to_DB();
 
-    Utente user;
-
-    user.nome="Florindo";
-    user.password="frociodimerda";
-    user.lingua="srilankese";
-
-    //login(conn,&user);
-
-    register_user(conn,&user);
->>>>>>> c409c2b5b7b82bf9ca566b88c23c05a0d329f410
 }
 
 
