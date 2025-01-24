d23299c8a08c5b10aa004c4ef7d87b63bc87a3ab
filix <florindozec@gmail.com>
2025-01-24 19:01:20 +0100

    errori fatti da git

--- main.c
+++ main.c
@@ -4,15 +4,11 @@
 
 #include "include/connectionDB.h"
 #include "include/auth.h"
-<<<<<<< HEAD
-#include "Prove Socket/server.c"
-=======
->>>>>>> c409c2b5b7b82bf9ca566b88c23c05a0d329f410
+#include "Prove giSocket/server.c"
 
 
 int main(){
 
-<<<<<<< HEAD
     
 
     //Utente user;
@@ -25,19 +21,6 @@ int main(){
 
     //register_user(conn,&user);
     mainServer();
-=======
-    PGconn* conn=connect_to_DB();
-
-    Utente user;
-
-    user.nome="Florindo";
-    user.password="frociodimerda";
-    user.lingua="srilankese";
-
-    //login(conn,&user);
-
-    register_user(conn,&user);
->>>>>>> c409c2b5b7b82bf9ca566b88c23c05a0d329f410
 }
 
 
