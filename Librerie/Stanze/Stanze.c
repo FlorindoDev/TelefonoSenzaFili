d28cb6e5d1a37ab09dbe1048f30f8f81fd0a381e
filix <florindozec@gmail.com>
2025-01-28 20:21:17 +0000

    aggiunto i fari mutex per la sicronizzazione dei processi(funzionano)

--- Librerie/Stanze/Stanze.c
+++ Librerie/Stanze/Stanze.c
@@ -2,6 +2,7 @@
 #include "Stanze.h"
 
 void inserisciStanza(ListStanze* liststanze, Stanza* new){
+    pthread_mutex_lock(&(liststanze->light));
     if(liststanze->next == NULL){
         new->next=NULL;
         liststanze->next = new;
@@ -9,27 +10,35 @@ void inserisciStanza(ListStanze* liststanze, Stanza* new){
         new->next=liststanze->next;
         liststanze->next=new;
     }
-
+    pthread_mutex_unlock(&(liststanze->light));
 
 }
 
 
 int existStanza(ListStanze* liststanze, Stanza* check){
 
-    if(liststanze->next == NULL) return 0;
+    pthread_mutex_lock(&(liststanze->light));
+    if(liststanze->next == NULL){
+        pthread_mutex_unlock(&(liststanze->light));
+        return 0;
+    }
     Stanza *tmp = liststanze->next;
     while(tmp != NULL){
-        if(strcmp(tmp->nomeStanza,check->nomeStanza) == 0)return 1;
+        if(strcmp(tmp->nomeStanza,check->nomeStanza) == 0){
+            pthread_mutex_unlock(&(liststanze->light));
+            return 1;
+        }
         tmp = tmp->next;
     }
 
+    pthread_mutex_unlock(&(liststanze->light));
     return 0;
 }
 
 ListStanze* initTesta(){
     ListStanze* listStanze = (ListStanze *)malloc(sizeof(ListStanze));
     listStanze->next = NULL; 
-    if (pthread_mutex_init(&listStanze->light, NULL) != 0) {
+    if (pthread_mutex_init(&(listStanze->light), NULL) != 0) {
         perror("Errore durante l'inizializzazione del mutex");
         exit(-1);
     }
@@ -38,23 +47,32 @@ ListStanze* initTesta(){
 
 
 int removeStanza(ListStanze* liststanze, char* nomeStanza){
-    if(liststanze->next == NULL) return 0;
+    
+    pthread_mutex_lock(&(liststanze->light));
+    if(liststanze->next == NULL){
+        pthread_mutex_unlock(&(liststanze->light));
+        return 0;
+    }
+
     Stanza *tmp = liststanze->next;
     Stanza *pre = NULL;
+    
     while(tmp != NULL){
         if(pre == NULL && strcmp(tmp->nomeStanza,nomeStanza)==0){
             liststanze->next = tmp->next;
             free(tmp);
+            pthread_mutex_unlock(&(liststanze->light));
             return 1;
         }else if(strcmp(tmp->nomeStanza,nomeStanza)==0){
             pre->next=tmp->next;
             free(tmp);
+            pthread_mutex_unlock(&(liststanze->light));
             return 1;
         }
         pre = tmp;
         tmp=tmp->next;
     }
-
+    pthread_mutex_unlock(&(liststanze->light));
     return 0;
 
 }
