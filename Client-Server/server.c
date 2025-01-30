1be3a5dce287b570101416607efb36376573ead4
GDom03 <domenicogagliotti3@gmail.com>
2025-01-30 21:22:14 +0100

    prova2

--- Client-Server/server.c
+++ Client-Server/server.c
@@ -5,7 +5,7 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 
-//Florindo e' belli
+//aertgaergaer
 
 #include "../Librerie/Autenticazione/auth.h"
 #include "../Librerie/connessioneDB/connectionDB.h"
